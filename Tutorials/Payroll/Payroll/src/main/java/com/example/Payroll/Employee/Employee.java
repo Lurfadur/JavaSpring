da4ca7b68764885a2bcb87e653d8704762a9237d
Graham Hough <ghough85@gmail.com>
2022-03-11 08:47:25 -0800

    continue tutorial

---
+++ Tutorials/Payroll/Payroll/src/main/java/com/example/Payroll/Employee/Employee.java
@@ -0,0 +1,12 @@
+package main.java.com.example.Payroll.Employee;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+class Employee {
+
+}
